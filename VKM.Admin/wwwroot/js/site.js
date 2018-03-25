239d1a4d37bcea50a62d99f2e20f42334203ec46
PicOLinO <webmaster.pico@gmail.com>
2018-03-25 15:14:01 +0300

    Корректирование верстки и добавление выделения

--- VKM.Admin/wwwroot/js/site.js
+++ VKM.Admin/wwwroot/js/site.js
@@ -1,3 +1,31 @@
 ﻿// Write your JavaScript code.
 
-$(function () { $('#BaseTree').jstree(); });
+$(function () {
+    $('#BaseTree').jstree(
+        {
+            "types": {
+                "team": {
+                    "icon": "glyphicon glyphicon-ok",
+                    "hover_node": false,
+                    "valid_children": ["student"]
+                },
+                "student": {
+                    "icon": "glyphicon glyphicon-flash",
+                    "valid_children": "none"
+                }
+            },
+            "conditionalselect": function (node, event) {
+                return node.type !== "team";
+            },
+            "plugins": ["conditionalselect", "sort", "wholerow", "unique", "types"]
+        }
+    );
+});
+$('#BaseTree')
+
+    .on('changed.jstree', function (e, data) {
+        $('#SelectedStudent').html(data.instance.get_node(data.selected[0]).text);
+    })
+    .on('loaded.jstree', function (e, data) {
+        data.instance.set_type()
+    });
